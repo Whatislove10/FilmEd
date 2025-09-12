e35bde9168c9807c7c26b4e685aae4b2f1b95b88
nikitastrekalov <nikitastrekalov2003@gmail.com>
2025-09-12 15:28:06 +0300

    feat(NikitaBR):delete

--- src/components/HomePage.js
+++
@@ -1,9 +0,0 @@
-// src/components/HomePage.js
-export const HomePage = () => {
-    return `
-        <div class="bg-[#2a2e33] p-10 rounded-lg text-center">
-            <h2 class="text-3xl font-bold text-white">Welcome to the Video Project!</h2>
-            <p class="mt-4 text-gray-400">This is a fully dynamic prototype. Click around to explore.</p>
-        </div>
-    `;
-};
